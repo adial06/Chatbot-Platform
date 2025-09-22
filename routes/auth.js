09c11f44ee4b755e0234c9c3bb195d0510f1e885
Aditi S Aravinda <124051804+adial06@users.noreply.github.com>
2025-09-23 01:01:00 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,1 +1,10 @@
+import { Router } from "express";
+import { registerUser, loginUser } from "../controllers/authcontroller.js";
+
+const router = Router();
+
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+
+export default router;
 
