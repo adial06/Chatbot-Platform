6030c6ba89f9ea13c23625300f7e344bf5a7c4a2
Aditi S Aravinda <124051804+adial06@users.noreply.github.com>
2025-09-23 00:55:19 +0530

    Delete chat.js

--- chat.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { chatWithProject } from "../controllers/chatcontrollers.js";
-
-const router = express.Router({ mergeParams: true });
-
-router.post("/:projectId/chat", chatWithProject);
-
-export default router;
